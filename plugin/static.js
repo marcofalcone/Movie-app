0e1b06720cbcc71b6d9f93d24ce8e7a1ea99a482
Marco <marco.falcone@redev.enterprises>
2022-11-04 22:48:34 +0100

    edit frontend route

--- plugin/static.js
+++ plugin/static.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const fastStatic = async (fastify) => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  const buildPath = path.join(__dirname, '../react/build')
+  const buildPath = path.join(__dirname, '../frontend/build')
   await fastify.register(fastifyStatic, {
     root: buildPath
   });
