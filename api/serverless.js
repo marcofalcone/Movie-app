54161f7718d817afb43ca467adf92edcee71518e
Marco <marco.falcone@redev.enterprises>
2022-11-08 00:07:58 +0100

    vercel config, followup

--- api/serverless.js
+++ api/serverless.js
@@ -13,7 +13,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("./dist"));
+app.register(import("../dist"));
 
 export default async (req, res) => {
     await app.ready();
